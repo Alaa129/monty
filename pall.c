b97d59c6c55d22a6d6dee1520df686a7df61cb51
Cornato <dragonroar0099@gmail.com>
2023-12-14 17:57:15 +0200

    test

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void f_pall(stack_b **head, unsigned int counter)
+{
+	stack_b *ssss;
+	(void)counter;
+
+	ssss = *head;
+	if (ssss == NULL)
+		return;
+	while (ssss)
+	{
+		printf("%d\n", ssss->n);
+		ssss = ssss->next;
+	}
+}
