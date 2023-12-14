14bad5b14032b8e93a7d642373b77b708f05cc5d
Alaa129 <alaa1292002.a@gmail.com>
2023-12-14 18:19:04 +0200

    task

--- monty.h
+++ monty.h
@@ -18,61 +18,30 @@
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_b;
 /**
- * struct bus_s - variables
+ * struct test_s - variables
  * @arg: va
  * @file: pointer
  * @content: line
  * @lifi: change
  * Description: carries
  */
-typedef struct bus_s
+typedef struct test_s
 {
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_b bus;
-/**
- * struct instruction_s - opcode
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode 
- * for stack, queues, LIFO, FIFO 
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_b;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+    char *arg;
+    FILE *file;
+    char *content;
+    int lifi;
+}  test_t;
+extern test_t test;
+
+void s_push(stack_b **head, unsigned int line_co);
+void s_pall(stack_b **head, unsigned int line_co);
+void free_stack(stack_b *head);
+void addnode(stack_b **head, int n);
+void addqueue(stack_b **head, int n);
 #endif
